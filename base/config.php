334e2e8290224c2a720d4e09decf45a8c5baedca
Acidburn <mikebocharov63@gmail.com>
2022-09-07 02:56:01 +0300

    first commit

---
+++ base/config.php
@@ -0,0 +1,16 @@
+<?php
+
+const DB_USER = 'root';
+const DB_NAME = 'test_api_json';
+const DB_HOST = 'localhost';
+const DB_PASSWORD = 'root';
+// admin ids - manual switch
+//const ADMIN_IDS = [9];
+
+
+//dumper for tests
+function d(...$args)
+{
+    var_dump($args);
+    die;
+}
